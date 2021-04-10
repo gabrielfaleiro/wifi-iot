71d47935c692863a3bcaab177082215420c508c8
gfaleiro <faleirogabrielf@gmail.com>
2021-04-10 18:40:47 +0200

    update- testing DHC11

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 
 #include "Arduino.h"
+// #include <ESP8266WiFi.h>
+
+#include <Adafruit_Sensor.h>
 #include "DHT.h"
 
 // Correct internal values. They work swapped for the LED_BUILTIN
-#define LOW 0x1
-#define HIGH 0x0
+// #define LOW 0x1
+// #define HIGH 0x0
 
 // Comment DEBUG definition to avoid preprocesor reporting info trough the Serial port
 #define DEBUG 1
 
 // Digital pin to read sensor
-#define DHTPIN D6
+#define DHTPIN D5
 // Sensor type
 #define DHTTYPE DHT11
 
@@ -18,7 +21,7 @@
 
 // GLOBAL VARIABLES ///////////////////////////////////////////////////////////
 
-// Temp RH DHT11 Sensor initialisation
+// DHT11 Sensor initialisation
 DHT dht(DHTPIN, DHTTYPE);
 
 // CUSTOM FUNCTIONS ///////////////////////////////////////////////////////////
@@ -29,20 +32,24 @@ void toggleLed(){
   digitalWrite(LED_BUILTIN, !value);
 }
 
-// #ifdef DEBUG
-void printDH11(float rh, float temp){
-  if (isnan(rh) || isnan(temp)) {
-    Serial.println("Error obteniendo los datos del sensor DHT11");
-    return;
-  }
-  else{
+void printDH11(){
+  float rh = 0;
+  float temp = 0;
+
+  rh = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  // if (isnan(rh) || isnan(temp)) {
+  //   Serial.println("Error obteniendo los datos del sensor DHT11");
+  //   return;
+  // }
+  // else{
     Serial.print("rh: ");
     Serial.println(rh);
     Serial.print("temp: ");
     Serial.println(temp);
-  }
+  // }
 }
-// #endif
 
 // SETUP //////////////////////////////////////////////////////////////////////
 
@@ -58,7 +65,7 @@ void setup()
 
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, HIGH); // LED is off when pin output is 3.3V
 
   pinMode(REED_SWITCH_PIN , INPUT_PULLUP); 
 }
@@ -67,20 +74,10 @@ void setup()
 
 void loop()
 {
-  unsigned long currentMillis = 0;
-
-  float rh = 0;
-  float temp = 0;
-
-  currentMillis = millis();
-
-  rh = dht.readHumidity();
-  temp = dht.readTemperature();
-
   // #ifdef DEBUG
-  printDH11(rh,temp);
+  printDH11();
   toggleLed();
   // #endif
 
-  delay(500);
+  delay(2000);
 }
